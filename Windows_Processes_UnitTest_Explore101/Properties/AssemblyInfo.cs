f86755ddff983c4782e1b43bb763ac3c059a2c15
Jin Vincent Necesario <lock29down@gmail.com>
2019-12-15 16:18:25 +0800

    Add project files.

---
+++ Windows_Processes_UnitTest_Explore101/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Windows_Processes_UnitTest_Explore101")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Windows_Processes_UnitTest_Explore101")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a5470a37-42d6-478c-8e85-2a7d87302c3a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Windows_Processes_UnitTest_Explore101/Windows_Processes_UnitTest.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Windows_Processes_UnitTest_Explore101
+{
+    [TestClass]
+    public class Windows_Processes_UnitTest
+    {
+     
+        [TestMethod]
+        public void Test_Enumerating_Running_Processes()
+        {
+            string localMachineName = Environment.MachineName;
+
+            var runningProcess = Process.GetProcesses(localMachineName);
+
+            foreach (var process in runningProcess)
+            {
+                string processName = process.ProcessName;
+                string processId = process.Id.ToString();
+
+                Console.WriteLine($"Process Id: {processId} Process Name: {processName}");
+
+                Assert.IsNotNull(processName);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Check_And_Inspect_A_Specific_Process()
+        { 
+            string localMachineName = Environment.MachineName;
+
+            var process = Process.GetProcesses(localMachineName)
+                                 .FirstOrDefault(p => p.ProcessName.Contains("chrome"));
+
+            if(process != null)
+            {
+                Assert.IsNotNull(process);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Process_Thread_Set()
+        {
+            string localMachineName = Environment.MachineName;
+
+            var process = Process.GetProcesses(localMachineName)
+                                 .FirstOrDefault(p => p.ProcessName.Contains("chrome"));
+
+            if (process != null)
+            {
+                foreach (ProcessThread thread in process.Threads)
+                {
+                    string outputString = string.Empty;
+
+                    outputString = $"Thread ID: {thread.Id} {Environment.NewLine}";
+                    outputString += $"Thread ID: {thread.StartTime.ToShortDateString()}";
+                    outputString += $"Priority: {thread.PriorityLevel}";
+
+                    Console.WriteLine(outputString);
+                }
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Win32Exception))]
+        public void Test_Process_Module_Set_Of_Details_1_Using_ProcessObject()
+        {
+            string localMachineName = Environment.MachineName;
+
+            var process = Process.GetProcesses(localMachineName)
+                                 .FirstOrDefault(p => p.ProcessName.Contains("chrome"));
+
+            if (process != null)
+            {
+                foreach (ProcessModule module in process.Modules)
+                {
+                    string outputString = string.Empty;
+
+                    outputString = $"Module Name: {module.ModuleName} {Environment.NewLine}";
+                    outputString += $"Module Filename: {module.FileName}";
+
+                    Console.WriteLine(outputString);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Test_Process_Module_Set_Of_Details_2_Using_ManagementObject()
+        {
+            string localMachineName = Environment.MachineName;
+
+            var process = Process.GetProcesses(localMachineName)
+                                 .FirstOrDefault(p => p.ProcessName.Contains("chrome"));
+
+            if (process != null)
+            {
+                string query = $"SELECT ProcessId, ExecutablePath, CommandLine FROM Win32_Process WHERE ProcessId={process.Id}" ;
+
+                using(var search = new ManagementObjectSearcher(query))
+                {
+                    using(var results = search.Get())
+                    {
+
+                        var processModuleDetails = results.Cast<ManagementObject>().FirstOrDefault();
+
+                        if (processModuleDetails != null)
+                        {
+                            var stringResult = $"Module Name: {processModuleDetails.GetPropertyValue("CommandLine")}{Environment.NewLine}";
+                                stringResult += $"Module Filename:{processModuleDetails.GetPropertyValue("ExecutablePath")}{Environment.NewLine}";
+                                stringResult += $"Process Id: {processModuleDetails.GetPropertyValue("ProcessId")}";
+
+
+                            Console.WriteLine(stringResult);
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestMethod]
+        public void Test_Process_Start_Process()
+        {
+            string acrobatReaderName = "AcroRd32";
+
+            try
+            {
+                /*
+                 * For the complete argument list see the link below.
+                 * https://www.adobe.com/content/dam/acom/en/devnet/acrobat/pdfs/pdf_open_parameters.pdf
+                 */
+
+                var acrobatReaderProcess = Process.Start(acrobatReaderName, "/n");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void Test_Process_Stop_Process()
+        {
+            string acrobatReaderName = "AcroRd32";
+
+            try
+            {
+                var acrobatReaderProcess = Process.GetProcessesByName(acrobatReaderName);
+
+                if (acrobatReaderProcess != null)
+                {
+                    foreach (var item in acrobatReaderProcess)
+                    {
+                        item.Kill();
+
+                        Console.WriteLine($"{item.ProcessName} has been killed!");
+
+                    }
+                }
+            }
+            catch (Win32Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
